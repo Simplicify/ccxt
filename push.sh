55fe960d4b57a27dd6a70eb9cc26f8e27fbf98ae
William Turner <william.turner@logmein.com>
2018-03-12 12:54:16 -0700

    Testing

--- push.sh
+++ push.sh
@@ -7,5 +7,5 @@ git add --force build/ccxt.browser.js
 git commit -a -m "${COMMIT_MESSAGE}" -m '[ci skip]'
 git tag -a "${COMMIT_MESSAGE}" -m "${LAST_COMMIT_MESSAGE}" -m "" -m "[ci skip]"
 git remote remove origin
-git remote add origin https://${GITHUB_TOKEN}wmturner@github.com/Simplicify/ccxt
+git remote add origin https://wmturner:pUCtPf9f7Ij9mRbItoVdJQ@github.com/Simplicify/ccxt
 git push origin --tags HEAD:master
